84ec40fb51a75ff198bfb075a14b503d8a674375
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-09-01 20:59:09 -0700

    components

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Header = () => {
+  return (
+    <Head>
+      <title>Underground Universal v0.1</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+  );
+};
+
+export default Header;
---
+++ components/Nav.tsx
@@ -0,0 +1,14 @@
+const LeftNav = () => {
+  return <div className="sm:hidden">LeftNav</div>;
+};
+
+const Nav = () => {
+  return (
+    <div>
+      <LeftNav />
+      Nav
+    </div>
+  );
+};
+
+export default Nav;
