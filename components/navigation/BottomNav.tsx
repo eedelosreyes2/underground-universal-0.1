8c22391ff7d806f04f698adfb0c9f1916d6413da
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-09-03 23:26:36 -0700

    bettr nav icons

--- components/navigation/BottomNav.tsx
+++ components/navigation/BottomNav.tsx
@@ -1,8 +1,8 @@
 import { IconContext } from 'react-icons';
-import { RiCompassDiscoverLine } from 'react-icons/ri';
 import { RiHeadphoneFill } from 'react-icons/ri';
-import { MdOutlineLibraryMusic } from 'react-icons/md';
+import { MdLibraryMusic } from 'react-icons/md';
 import { MdPeopleAlt } from 'react-icons/md';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const BottomNav = () => {
   return (
@@ -11,22 +11,22 @@ const BottomNav = () => {
       flex justify-evenly align-middle
       bg-nav-light dark:bg-nav-dark sm:hidden"
     >
-      <div className="p-3 px-6">
-        <IconContext.Provider value={{ size: '1.75em' }}>
-          <RiCompassDiscoverLine />
+      <div className="p-3 pt-4">
+        <IconContext.Provider value={{ size: '1.35em' }}>
+          <FaLocationArrow />
         </IconContext.Provider>
       </div>
-      <div className="p-3 px-6">
+      <div className="p-3">
         <IconContext.Provider value={{ size: '1.75em' }}>
           <RiHeadphoneFill />
         </IconContext.Provider>
       </div>
-      <div className="p-3 px-6">
+      <div className="p-3">
         <IconContext.Provider value={{ size: '1.75em' }}>
-          <MdOutlineLibraryMusic />
+          <MdLibraryMusic />
         </IconContext.Provider>
       </div>
-      <div className="p-3 px-6">
+      <div className="p-3">
         <IconContext.Provider value={{ size: '1.75em' }}>
           <MdPeopleAlt />
         </IconContext.Provider>
