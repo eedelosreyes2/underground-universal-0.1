7904c2f668a0030ef9fd97e88d5f38a569eef9b7
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-09-07 21:08:38 -0700

    prisma, schemas, data

---
+++ data/artists.tsx
@@ -0,0 +1,24 @@
+// Will get data from here until Prisma is set up
+
+export const artists = [
+  {
+    id: 0,
+    name: 'Joe',
+    location: 'San Francisco, CA',
+  },
+  {
+    id: 1,
+    name: 'Mama',
+    location: 'Los Angeles, CA',
+  },
+  {
+    id: 2,
+    name: 'Suzy',
+    location: 'New York, NY',
+  },
+  {
+    id: 3,
+    name: 'Llama',
+    location: 'Chicago, IL',
+  },
+];
