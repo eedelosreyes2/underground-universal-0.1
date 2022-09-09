050783611e7864ef347c13fbd9454982d5d65e82
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-09-09 00:28:02 -0700

    graphql artist schema

--- graphql/types/Artist.ts
+++ graphql/types/Artist.ts
@@ -1,38 +1,64 @@
-import { objectType, extendType } from 'nexus';
+import { objectType } from 'nexus';
 import { Status, Role, Level, Genre } from './Enums';
+import { Album } from './Album';
+import { Badge } from './Badge';
 import { Track } from './Track';
+import { Studio } from './Studio';
 
 export const Artist = objectType({
   name: 'Artist',
   definition(t) {
     t.string('id');
-    // t.string('createdAt');
-    // t.string('udpatedAt');
+    t.string('createdAt');
+    t.string('udpatedAt');
     t.string('email');
-    // t.string('dob');
+    t.string('dob');
     t.string('name');
     t.string('location');
     t.string('badgeId');
     t.string('trackId');
     t.field('role', { type: Role });
     t.field('level', { type: Level });
-    // t.string('genres');
+    t.list.field('genres', { type: Genre });
     t.string('bio');
-    // t.string('streaming');
-    // t.string('status');
-    // t.string('trackSig');
-    // t.string('albums');
-    // t.string('badge');
-    // t.string('tracks');
-    // t.string('studios');
-    // t.string('features');
-    // t.string('sharedAlbum');
-    // t.string('sharedAlbumWith');
-    // t.string('sharedTrack');
-    // t.string('sharedTrackWith');
-    // t.string('Artist_B');
-    // t.string('Artist_A');
-
+    t.list.string('streaming');
+    t.field('status', { type: Status });
+    t.field('trackSig', {
+      type: Track,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.trackId,
+            },
+          })
+          .albums();
+      },
+    });
+    t.list.field('albums', {
+      type: Album,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .albums();
+      },
+    });
+    t.list.field('badges', {
+      type: Badge,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.badgeId,
+            },
+          })
+          .badges();
+      },
+    });
     t.list.field('tracks', {
       type: Track,
       async resolve(_parent, _args, ctx) {
@@ -43,6 +69,102 @@ export const Artist = objectType({
             },
           })
           .tracks();
+      },
+    });
+    t.list.field('studios', {
+      type: Studio,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .studios();
+      },
+    });
+    t.list.field('features', {
+      type: Artist,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .features();
+      },
+    });
+    t.list.field('sharedAlbum', {
+      type: Album,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .sharedAlbum();
+      },
+    });
+    t.list.field('sharedAlbumWith', {
+      type: Studio,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .sharedAlbumWith();
+      },
+    });
+    t.list.field('sharedTrack', {
+      type: Track,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .sharedTrack();
+      },
+    });
+    t.list.field('sharedTrackWith', {
+      type: Track,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .sharedTrackWith();
+      },
+    });
+    t.list.field('collabed', {
+      type: Artist,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .collabed();
+      },
+    });
+    t.list.field('collabedWith', {
+      type: Artist,
+      async resolve(_parent, _args, ctx) {
+        return await ctx.prisma.artist
+          .findUnique({
+            where: {
+              id: _parent.id!,
+            },
+          })
+          .collabedWith();
       },
     });
   },
