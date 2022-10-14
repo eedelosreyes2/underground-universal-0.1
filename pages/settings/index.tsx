b218d2631366f7c690bb965d668d17fb2eb32f91
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-10-13 19:14:21 -0700

    auth redirercts

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -15,7 +15,7 @@ export const getServerSideProps = async ({
     return {
       redirect: {
         permanent: false,
-        destination: '/api/auth/login',
+        destination: '/',
       },
       props: {},
     };
--- pages/settings/profile.tsx
+++ pages/settings/profile.tsx
@@ -1,5 +1,30 @@
+import { getSession } from '@auth0/nextjs-auth0';
 import Layout from '../../components/Layout';
 import Profile from '../../components/settings/Profile';
+
+export const getServerSideProps = async ({
+  req,
+  res,
+}: {
+  req: any;
+  res: any;
+}) => {
+  const session = getSession(req, res);
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+      props: {},
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 const editProfile = () => {
   return (
