58637a7627672e5d04890cf0408e05e189dd26d8
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-11-03 19:48:54 -0700

    montserrat

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,10 @@ const Header = () => {
     <Head>
       <title>Underground Universal v0.1</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+        rel="stylesheet"
+      ></link>
     </Head>
   );
 };
