77bd123aa36891080d50a967f8f048916f40b8b2
eedelosreyes2 <delosreyes.elijah@gmail.com>
2023-01-09 21:30:33 -0800

    collabed + messaging

--- components/collabs/Collabs.tsx
+++ components/collabs/Collabs.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { IconContext } from 'react-icons';
 import { HiOutlineAtSymbol } from 'react-icons/hi';
-import { IoCheckmarkOutline, IoCloseOutline } from 'react-icons/io5';
+import {
+  IoChatboxOutline,
+  IoCheckmarkOutline,
+  IoCloseOutline,
+} from 'react-icons/io5';
 
 const GET_COLLABS = gql`
   query ($email: String!) {
@@ -48,12 +52,14 @@ const Collabs = () => {
 
   const handleRemoveSent = (artist: any) => {
     if (confirm('Cancel Collab request to ' + artist.name + '?')) {
+      // TODO
       console.log('remove ' + artist.name + ' from collabsSent');
     }
   };
 
   const handleAcceptReceieved = (artist: any) => {
     if (confirm('Accept Collab request from ' + artist.name + '?')) {
+      // TODO
       console.log(
         'add ' + artist.name + ' to collabsSent as well as collabsReceived'
       );
@@ -62,8 +68,18 @@ const Collabs = () => {
 
   const handleRejectReceieved = (artist: any) => {
     if (confirm('Reject Collab request from ' + artist.name + '?')) {
+      // TODO
       console.log('remove ' + artist.name + ' from collabsReceived');
     }
+  };
+
+  const handleSendMessage = (artist: any) => {
+    // TODO
+    alert(
+      'The Messaging Collabs feature is not yet available... For now you can reach out to ' +
+        artist.name +
+        ' through their Discord/Email \n\nJoin the Underground Universal Discord to get updates on when this feature will be ready!'
+    );
   };
 
   const renderCollab = (artist: any, type: string) => {
@@ -100,6 +116,7 @@ const Collabs = () => {
               />
             </IconContext.Provider>
           )}
+
           {type === 'receieved' && (
             <IconContext.Provider value={{ size: '1.2em' }}>
               <div className="flex gap-5">
@@ -112,6 +129,15 @@ const Collabs = () => {
                   className="cursor-pointer"
                 />
               </div>
+            </IconContext.Provider>
+          )}
+
+          {type === 'collabed' && (
+            <IconContext.Provider value={{ size: '1.2em' }}>
+              <IoChatboxOutline
+                onClick={() => handleSendMessage(artist)}
+                className="cursor-pointer"
+              />
             </IconContext.Provider>
           )}
         </div>
@@ -130,10 +156,18 @@ const Collabs = () => {
       </div>
 
       <h3 className="mb-3">Received</h3>
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 mb-10">
         {collabsReceived &&
           Object.values(collabsReceived).map((artist: any) =>
             renderCollab(artist, 'receieved')
+          )}
+      </div>
+
+      <h3 className="mb-3">Collabed</h3>
+      <div className="flex flex-col gap-3 mb-10">
+        {collabsReceived &&
+          Object.values(collabsReceived).map((artist: any) =>
+            renderCollab(artist, 'collabed')
           )}
       </div>
     </div>
