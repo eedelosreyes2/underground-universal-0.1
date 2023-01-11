73bbfd54aee4753d6c812e15a283847135281700
eedelosreyes2 <delosreyes.elijah@gmail.com>
2023-01-11 02:15:14 -0800

    library

---
+++ components/library/Library.tsx
@@ -0,0 +1,20 @@
+const Library = () => {
+  return (
+    <div className="flex flex-col gap-5 justify-start w-full">
+      <div>
+        This feature is not yet available... Join our{' '}
+        <a
+          className="text-button"
+          href="https://discord.com/invite/KNUG3yTsT8/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Discord
+        </a>{' '}
+        to get the latest updates.
+      </div>
+    </div>
+  );
+};
+
+export default Library;
