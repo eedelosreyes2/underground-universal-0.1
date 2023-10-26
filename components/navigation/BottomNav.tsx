cd53ee92132e57530765d021c4a3a5e454704400
Elijah Delos Reyes <Elijah.DelosReyes@roberthalf.com>
2023-10-26 14:00:28 -0700

    bottom nav

--- components/navigation/BottomNav.tsx
+++ components/navigation/BottomNav.tsx
@@ -8,11 +8,11 @@ const BottomNav = () => {
     <div
       className="fixed bottom-0 right-0 left-0 
       flex justify-evenly align-middle
-      bg-nav-light dark:bg-nav-dark md:hidden"
+      bg-nav-light dark:bg-nav-dark md:hidden py-2"
     >
       <ActiveLink href="/discover">
-        <div className="p-3 pt-4">
-          <IconContext.Provider value={{ size: "1.35em" }}>
+        <div className="p-3 scale-125">
+          <IconContext.Provider value={{}}>
             <FaLocationArrow />
           </IconContext.Provider>
         </div>
@@ -35,16 +35,16 @@ const BottomNav = () => {
       </ActiveLink> */}
 
       <ActiveLink href="/collabs">
-        <div className="p-3">
-          <IconContext.Provider value={{ size: "1.75em" }}>
+        <div className="p-3 scale-150">
+          <IconContext.Provider value={{}}>
             <MdPeopleAlt />
           </IconContext.Provider>
         </div>
       </ActiveLink>
 
       <ActiveLink href="/settings">
-        <div className="p-3">
-          <IconContext.Provider value={{ size: "1.75em" }}>
+        <div className="p-3 scale-150">
+          <IconContext.Provider value={{}}>
             <MdHelp />
           </IconContext.Provider>
         </div>
